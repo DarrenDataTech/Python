7eff391662e003ef8b29db92df6e765c9e723115
Darren Patterson <darren.patterson@hotmail.co.uk>
2025-05-14 18:32:13 +0100

    Finishing While Loops.

---
+++ Automate_the_Boring_Stuff_with_Python/Section2-Flow_Control/spam.py
@@ -0,0 +1,7 @@
+spam = 0
+while span < 5:
+    spam = spam + 1
+    if spam == 3:
+        continue
+    print('spam is ' + str(spam))
+# The code above prints the value of `spam` from 1 to 5, but skips the print statement when `spam` is equal to 3. The `continue` statement causes the loop to skip the current iteration and move to the next one.
---
+++ Automate_the_Boring_Stuff_with_Python/Section2-Flow_Control/yourName.py
@@ -0,0 +1,6 @@
+name = ''
+while name != 'Darren':
+    print ('Please type your name...')
+    name = input()
+print ('Thank you!')
+# The code above repeatedly prompts the user to type their name until they enter 'Darren'. Once 'Darren' is entered, it prints 'Thank you!'.dave
