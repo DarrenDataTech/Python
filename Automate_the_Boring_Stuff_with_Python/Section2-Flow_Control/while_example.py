ff325f053d433c74b1d22c2828d1c4ced7c50843
Darren Patterson <darren.patterson@hotmail.co.uk>
2025-06-02 18:16:46 +0100

    :)

--- Automate_the_Boring_Stuff_with_Python/Section2-Flow_Control/while_example.py
+++ Automate_the_Boring_Stuff_with_Python/Section2-Flow_Control/while_example.py
@@ -2,4 +2,4 @@ spam = 0
 while spam < 5 :
     print('Hello, world!')
     spam = spam + 1
-# The code above prints 'Hello, world!' five times. It uses a while loop that continues as long as `spam` is less than 5. After each print, it increments `spam` by 1.
+# The code above prints 'Hello, world!' five times. It uses a while loop that continues as long as `spam` is less than 5. After each print, it increments `spam` by 1.  
